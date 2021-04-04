add39f1ae0741bd07b17aca1692afa185c923f9c
Nicholas de Jong <me@nicholasdejong.com>
2021-04-04 20:23:55 +1000

    (elasticsearch_kibana_cli) bump version to 0.3.1

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ requirements = [
 
 setuptools.setup(
   name = 'elasticsearch_kibana_cli',
-  version = '0.3.0',
+  version = '0.3.1',
   author = 'Nicholas de Jong',
   author_email = 'contact@nicholasdejong.com',
   description = 'CLI interface to query Elasticsearch backend via the Kibana frontend.',
